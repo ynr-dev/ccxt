8aaa6d0ce91c0ab7a8a22b18b1c48439e5999ea5
hong9 <dev@qemistry.io>
2023-12-05 10:13:38 +0900

    ccxt public 기능 개발

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,20 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
+     const app = await NestFactory.create<NestFastifyApplication>(
+       AppModule,
+      new FastifyAdapter(),
 
-  );
-  app.enableCors(
-      {
-        origin : "*"
-      }
-
-  );
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+    );
+    const corsOptions: CorsOptions = {
+        origin: '*', // 프론트엔드 앱의 도메인을 지정
+        credentials: true, // 인증 정보 (쿠키 등)를 포함할 경우 true로 설정
+    };
+    app.enableCors(corsOptions);
+    app.useWebSocketAdapter(new WsAdapter(app));
+    await app.listen(3000);
 }
 bootstrap();
