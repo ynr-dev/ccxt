62f5b9b08797e823fe2f8bed3f2c8cc6f034bc5a
hong9 <dev@qemistry.io>
2024-02-01 01:41:29 +0900

    gopax 개발

--- src/ws/ws.gopax.ts
+++ src/ws/ws.gopax.ts
@@ -1,4 +1,5 @@
 import {Injectable} from "@nestjs/common";
+import {WebSocket} from "ws";
 
 @Injectable()
 export class GopaxService {
@@ -24,17 +25,23 @@ export class GopaxService {
     async fetchOHLCV(symbol: string, timeframe: string, since: number):Promise<any>{
         // const strArr = symbols.split(',');
         const interval = this.getInterval(timeframe);
-
+        console.log(new Date(since))
+        console.log(interval)
         const axios = require('axios');
-        const response = await axios.get('https://api.gopax.co.kr/trading-pairs/'+symbol.replace("/", "-")+'/candles?start='+(since-1000*60*interval*1000)+'&end='+since+'&interval='+interval);
+        const response = await axios.get('https://api.gopax.co.kr/trading-pairs/'+symbol.replace("_", "-").toLocaleUpperCase()+'/candles?start='+(since-1000*60*interval*100)+'&end='+since+'&interval='+interval);
         const data = response.data;
 
+        data.forEach((item: any) =>{
+            console.log(new Date(item[0]))
+            console.log(item[0])
+
+        })
         return data.map((item: any) => [item[0], item[3], item[2], item[1], item[4], item[5]])
     }
 
     async fetchTimeframes(exchangeId: string, symbol: string): Promise<any> {
         const axios = require('axios');
-        const response = await axios.get('https://api.gopax.co.kr/trading-pairs/'+symbol.replace("/", "-")+'/ticker');
+        const response = await axios.get('https://api.gopax.co.kr/trading-pairs/'+symbol.replace("_", "-").toLocaleUpperCase()+'/ticker');
         const data:any = response.data;
         const price = data.price?.toString();
 
@@ -61,6 +68,40 @@ export class GopaxService {
             default:
                 return 1440;
         }
+    }
+
+    async wsOHLCV(timeframe:string, symbol:string, client:WebSocket){
+        const tradingPairName = symbol.replace("_", "-").toLocaleUpperCase()
+        const ws = new WebSocket("wss://wsapi.gopax.co.kr");
+        ws.onopen = () =>{
+            ws.send(JSON.stringify({
+
+                n : "SubscribeToTradingPair",
+                o: { tradingPairName},
+
+            }))
+        }
+
+        ws.onmessage =(message) =>{
+            const exchange = "gopax"
+            const ticker = symbol
+            if (typeof message.data === "string") {
+                const data = JSON.parse(message.data)
+                const result = data.o;
+                if(data == -1){
+
+                    const kline = [(result.t)*1000, result.o, result.h, result.l, result.c, result.a]
+                    client.send(JSON.stringify({exchange, ticker, timeframe, kline }));
+                }
+
+
+            }
+        }
+
+        client.on('close', function close() {
+            ws.close();
+        });
+
     }
 
 }
